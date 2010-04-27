b0aed16969dedc2bcf5e644a015e849f9826e675
Ted Price <ted.price@gmail.com>
2010-04-26 22:49:40 -0700

    Added paperclip mods

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,6 @@ class Offer < ActiveRecord::Base
   belongs_to :category
   has_many :comments
   belongs_to :business
+
+  has_attached_file :coupon, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end

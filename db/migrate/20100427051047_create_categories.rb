1df6327e43956c2350cc47be2bb9ba35f3f44136
Ted Price <ted.price@gmail.com>
2010-04-26 22:26:53 -0700

    + Default categories to active, delete gedit scraps

--- db/migrate/20100427051047_create_categories.rb
+++ db/migrate/20100427051047_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration
   def self.up
     create_table :categories do |t|
       t.string :name
-      t.boolean :active
+      t.boolean :active, :default => true
 
       t.timestamps
     end

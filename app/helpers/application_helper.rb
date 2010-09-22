e215b387bf0d7afe5ae58bae19cedcb3708d428c
Stefan Wrobel <swrobel@ubuntu.(none)>
2010-09-22 16:54:08 -0700

    Apply new my deals gallery style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,32 +5,23 @@ module ApplicationHelper
   end
 
   def coupon_body(offer)
-     "<span id='#{offer.id}_lead' class='subttl'>#{offer.lead}</span>
-     <p id='#{offer.id}_diz'>#{offer.description}  Offer expires <span id='#{offer.id}_ed'>#{offer.expiry_datetime.try(:strftime,('%b. %d, %Y'))}<span></p>
-     <div class='code'>
-       <p id='#{offer.id}_rc'>#{offer.redemption_code}</p>
-     </div>"
+     "<span id='#{offer.id}_lead'>#{offer.lead}</span>"
   end
 
   def deal_coupon(offer)
+    link_to(
     "
-    <div class='coupon'>
-      <h4>COUPON</h4>
-        <div class='holder'>
-          <div class='center'>
-            <div class='add-logo'>
-            " +
-      link_to(image_tag(offer.coupon.url, :width => 120, :height => 120), viewdeal_url(offer.to_param),
+    <div class='img_box'>
+      " +
+      image_tag(offer.coupon.url(:thumb)) +
+    "
+    </div>
+    <strong>COUPON</strong>
+    #{coupon_body offer}
+    ", viewdeal_url(offer.to_param),
               :onMouseOver => "$('offer_info_rollover').update('#{info_for_rollover(offer)}');",
               :onMouseOut => "$('offer_info_rollover').update('');"
-             )+
-              "
-            </div>
-          </div>
-       </div>
-       #{coupon_body offer}
-     </div>
-    "
+             )
   end
 
   def coupon(offer)
@@ -66,6 +57,7 @@ module ApplicationHelper
     link_to_remote(image_tag('/images/btn-good.gif', :alt => "+"),
                    :url => { :action => "set_opinion", :offer_id => offer.id, :liked => 'true' }) +
     "</span>" +
+    '<a href="#" class="link">Use It Now</a>' +
     "<span onMouseOver=\"$('offer_info_rollover').update('"+ CGI.escapeHTML(info_for_rollover(offer))+ "');\"
            onMouseOut=\"$('offer_info_rollover').update('')\" >" +
     link_to_remote(image_tag('/images/btn-bad.gif', :alt => "+"),

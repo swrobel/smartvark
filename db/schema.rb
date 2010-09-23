0e0bf94a9a32593d79446fa520ce6c533c73dfe9
Ted Price <ted.price@gmail.com>
2010-09-23 00:03:23 -0700

    Archived default to false

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file,
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
 # please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(:version => 20100916043654) do
     t.integer  "coupon_file_size"
     t.datetime "coupon_updated_at"
     t.string   "redemption_code",     :limit => 50
-    t.boolean  "archived",                            :default => true
+    t.boolean  "archived",                            :default => false
     t.date     "offer_active_on"
     t.string   "offer_type",          :limit => 20
     t.boolean  "allow_print"

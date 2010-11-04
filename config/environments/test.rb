829cc1404e567c25ca580c89319c31e87c9920d7
Stefan Wrobel <swrobel@ubuntu.(none)>
2010-11-04 00:29:31 -0700

    Updates for MoonshadoSMS support

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,3 +26,10 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
+
+# Don't send SMS except in production
+config.after_initialize do
+  Moonshado::Sms.configure do |config|
+    config.production_environment = false
+  end
+end

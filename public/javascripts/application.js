5bdfec087c2ceb780f2448d3325ea127b1b81b88
Stefan Wrobel <swrobel@ubuntu.(none)>
2010-12-03 04:38:51 -0800

    Clean up application.js and add slide effect to flash notices and fade effect to modals

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,90 +1,85 @@
-jQuery.noConflict();
-jQuery(document).ready(function(){
-	jQuery('ul.accordion').accordion({
+$(function(){
+	$('.accordion').accordion({
 		active: ".selected",
 		autoHeight: false,
 		header: ".opener",
 		collapsible: true,
 		event: "click"
 	});
+	
+	$.extend($.modal.defaults, {
+	    onOpen: function (dialog) {
+	        dialog.overlay.fadeIn('fast');
+	        dialog.container.fadeIn('fast');
+	        dialog.data.fadeIn('fast');
+		}, onClose: function (dialog) {
+	        dialog.data.fadeOut('fast');
+		    dialog.container.fadeOut('fast');
+			dialog.overlay.fadeOut('fast', function () {$.modal.close();});
+        }
+    });
 
-	jQuery('ul.accordion2').accordion({
-		autoHeight: false,
-		header: ".opener",
-		collapsible: true,
-		fillSpace: true,
-		event: "click"
-	});
-
-});
-jQuery(function (jQuery) {
-	jQuery('#btn-signin-open').click(function (e) {
-		jQuery('#signin-popup').modal();
+	$('#btn-signin-open').click(function (e) {
+		$('#signin-popup').modal();
 		  return false;
 	});
-});
-jQuery(function (jQuery) {
-	jQuery('#btn-signup-open').click(function (e) {
-		jQuery('#signup-popup').modal();
+    
+    $('#btn-signup-open').click(function (e) {
+		$('#signup-popup').modal();
 		  return false;
 	});
-});
-/********HACK  BEGIN****/
-//If user not logged in, and hits redeem show signin
-jQuery(function (jQuery) {
-	jQuery('#btn-redeem-signup-open').click(function (e) {
-		jQuery('#signup-popup').modal();
+	
+	$('#btn-redeem-signup-open').click(function (e) {
+		$('#signup-popup').modal();
 		  return false;
 	});
-});
-/********HACK  END****/
-jQuery(function (jQuery) {
-	jQuery('#btn-redeem-open').click(function (e) {
-		jQuery('#redeem-popup').modal();
+	
+	$('#btn-redeem-open').click(function (e) {
+		$('#redeem-popup').modal();
 		  return false;
 	});
-});
-jQuery(function (jQuery) {
-	jQuery('#signin-from-signup').click(function () {
-		jQuery.modal.close();
-		window.setTimeout(function() {jQuery('#signin-popup').modal()}, 250);
+	
+	$('#signin-from-signup').click(function () {
+		$.modal.close();
+		window.setTimeout(function() {$('#signin-popup').modal()}, 500);
 		  return false;
 	});
-});
-jQuery(function (jQuery) {
-	jQuery('#signup-from-signin').click(function () {
-		jQuery.modal.close();
-		window.setTimeout(function() {jQuery('#signup-popup').modal()}, 250);
+	
+	$('#signup-from-signin').click(function () {
+		$.modal.close();
+		window.setTimeout(function() {$('#signup-popup').modal()}, 500);
 		  return false;
 	});
-});
-jQuery(function() {
-	jQuery("#offer_offer_active_on").datepicker();
-	jQuery("#offer_expiry_datetime").datepicker();
-});
-jQuery(function() {
+	
+	$("#offer_offer_active_on").datepicker();
+	$("#offer_expiry_datetime").datepicker();
+	
 	//abort if browser supports HTML5 placeholder attribute
-	if ('placeholder' in document.createElement('input')) return;
-	jQuery("#search_terms").placeHeld();
-	jQuery("#location").placeHeld();
-	jQuery("#offer_lead").placeHeld();
-	jQuery("#offer_description").placeHeld();
-	jQuery("#offer_exclusivity_text").placeHeld();
-});
-jQuery(function (jQuery) {
-    jQuery(".dashboard_chk").change(function(){
-        isChecked = jQuery(this).attr("checked");
-        locName = jQuery(this).parent().text().trim();
-        jQuery(".location").find("td").each(function(){
-            if(jQuery(this).text() == locName) if(isChecked) jQuery(this).parent().show(); else jQuery(this).parent().hide();
+	if (!('placeholder' in document.createElement('input'))) {
+	    $("#search_terms").placeHeld();
+	    $("#location").placeHeld();
+	    $("#offer_lead").placeHeld();
+	    $("#offer_description").placeHeld();
+	    $("#offer_exclusivity_text").placeHeld();
+	}
+	
+	$(".dashboard_chk").change(function(){
+        isChecked = $(this).attr("checked");
+        locName = $(this).parent().text().trim();
+        $(".location").find("td").each(function(){
+            if($(this).text() == locName) if(isChecked) $(this).parent().show(); else $(this).parent().hide();
         });
     });
+    
+    if ($(".flash-holder").html() != "") {
+        $(".flash-holder").show("drop", { direction: "up" }, 1000).delay(5000).hide("drop", { direction: "up" }, 1000);
+    }
 });
 function checkall_offer_form(n) {
-	jQuery(".location_chk").attr('checked',n);
+	$(".location_chk").attr('checked',n);
 }
 function checkall(n) {
-	jQuery(".chk").attr('checked',n);
-	if (n) jQuery(".dashboard_row").show();
-	else jQuery(".dashboard_row").hide();
+	$(".chk").attr('checked',n);
+	if (n) $(".dashboard_row").show();
+	else $(".dashboard_row").hide();
 }

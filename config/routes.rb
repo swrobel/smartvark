8bef02439fac900165836a0d8a5f5853c2d44cba
Stefan Wrobel <swrobel@ubuntu.(none)>
2010-12-28 10:57:36 -0800

    Mobile signup/signin

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,12 @@ Smartvark::Application.routes.draw do
   match 'biz' => 'welcome#forbusiness', :as => :biz
   match 'about' => 'welcome#about', :as => :about
   match 'privacy' => 'welcome#privacy_policy', :as => :privacy
-  match 'contact' => 'welcome#contact', :as => :contact, :via => :get
-  match 'admin' => 'offers#index', :as => :admin
-  match 'mydeals/:category_id' => 'welcome#mydeals', :as => :category_mydeals
+  #match 'contact' => 'welcome#contact', :as => :contact
+  #match 'admin' => 'offers#index', :as => :admin
   match 'dashboard' => 'welcome#dealdashboard', :as => :dealdashboard
   match 'deals' => 'welcome#deals', :as => :deals
   match 'mydeals' => 'welcome#mydeals', :as => :mydeals
+  match 'mydeals/:category_id' => 'welcome#mydeals', :as => :category_mydeals
   match 'myprofile' => 'welcome#myprofile', :as => :myprofile
   match 'deal/:id' => 'welcome#viewdeal', :as => :viewdeal
   match 'printdeal/:id' => 'welcome#printdeal', :as => :printdeal
@@ -23,4 +23,6 @@ Smartvark::Application.routes.draw do
   match 'shout/:id' => 'welcome#shout', :as => :shout, :via => :post
   match 'set_opinion' => 'welcome#set_opinion', :as => :set_opinion, :via => :post
   match 'undo_last_action' => 'welcome#undo_last_action', :as => :undo_last_action, :via => :post
+  match 'signin' => 'welcome#signin', :as => :signin
+  match 'signup' => 'welcome#signup', :as => :signup
 end

316f796e4cfd6661d967348c67f0040226ac3f89
Stefan Wrobel <swrobel@ubuntu.(none)>
2011-01-02 22:11:56 -0800

    Clean up remaining issues with many-to-many biz-offers

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,13 +1,13 @@
 module WelcomeHelper
 
   def info_for_rollover(offer)
-    escape_javascript("<strong class=\"title\">#{offer.businesses.first.name}: #{offer.title}</strong><p>#{offer.description}</p><p>#{offer.fine_print}</p><p>#{offer.businesses.first.city}, #{offer.businesses.first.state} #{offer.businesses.first.zipcode}</p><p>Expires #{std_date(offer.end_date)}</p>")
+    escape_javascript("<strong class=\"title\">#{offer.user.name}: #{offer.title}</strong><p>#{offer.description}</p><p>#{offer.fine_print}</p><p>Expires #{std_date(offer.end_date)}</p>")
   end
 
   def list_offer(offer)
     "<li id='my_offer_#{offer.id}'>" +
     hidden_field_tag("user[offer_ids][]", offer.id) +
-    link_to("<h3>#{offer.businesses.first.name}: #{offer.title}</h3>".html_safe, viewdeal_path(offer),
+    link_to("<h3>#{offer.user.name}: #{offer.title}</h3>".html_safe, viewdeal_path(offer),
       :onMouseOver => "$('#offer_info_rollover').html('#{info_for_rollover(offer)}');",
       :onMouseOut => "$('#offer_info_rollover').empty();"
     ) +

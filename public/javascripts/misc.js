00eeba2e09e8fa53867f8bd4c9ec99970b0b347a
Stefan Wrobel <swrobel@ubuntu.(none)>
2011-04-05 23:10:24 -0700

    Use google bubble notifier to show users how to bookmark app to iPhone home screen

--- public/javascripts/misc.js
+++
@@ -1,7 +0,0 @@
-function remote_function(select, link) {
- $.post(link + "/" + retrieve_option_value(select), "", null, "script");
-}
-
-function retrieve_option_value(select) {
-  $(select).children(":selected").val();
-}

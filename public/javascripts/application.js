9fca370e6eb15b61920e55cd8a47600c5717f85d
Stefan Wrobel <swrobel@vm.(none)>
2011-05-02 21:40:00 -0700

    Move video from lightbox to landing page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -53,11 +53,6 @@ $(function(){
 			  return false;
 		});
 		
-		$('#btn-video-open').click(function (e) {
-			$('#video-popup').modal();
-			  return false;
-		});
-		
 		$('#signin-from-signup').click(function () {
 			$.modal.close();
 			window.setTimeout(function() {$('#signin-popup').modal()}, 500);

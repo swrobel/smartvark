92d330fb8a0e63e089f17ace48d59457a60787b5
Stefan Wrobel <stefan@smartvark.com>
2011-06-03 02:10:19 -0700

    Major overhaul to signin/up for mobile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,15 +3,16 @@ class RegistrationsController < Devise::RegistrationsController
   # POST /resource/sign_up
   def create
     build_resource
-
+    
     if resource.save
-      if resource.active?
-        set_flash_message :notice, :signed_up
-        sign_in_and_redirect(resource_name, resource)
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => redirect_location(resource_name, resource)
       else
-        set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s
+        set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s if is_navigational_format?
         expire_session_data_after_sign_in!
-        redirect_to after_inactive_sign_up_path_for(resource)
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
       end
     else
       flash[:alert] = "Could not complete signup. See errors below."
@@ -21,16 +22,17 @@ class RegistrationsController < Devise::RegistrationsController
       end
       flash[:error] += "</ul>"
       clean_up_passwords(resource)
-      redirect_to session[:user_return_to] || home_path
+      respond_with_navigational(resource) { render_with_scope :new }
+      #redirect_to session[:user_return_to] || home_path
     end
   end
   
   # PUT /resource
   def update
     if resource.update_with_password(params[resource_name])
-      set_flash_message :notice, :updated
+      set_flash_message :notice, :updated if is_navigational_format?
       sign_in resource_name, resource, :bypass => true
-      redirect_to after_update_path_for(resource)
+      respond_with resource, :location => after_update_path_for(resource)
     else
       flash[:alert] = "Could not update your account. See errors below."
       flash[:error] = "<ul>"
@@ -39,8 +41,10 @@ class RegistrationsController < Devise::RegistrationsController
       end
       flash[:error] += "</ul>"
       clean_up_passwords(resource)
-      redirect_to session[:user_return_to] || home_path
+      respond_with_navigational(resource){ render_with_scope :edit }
     end
+    
+    #redirect_to session[:user_return_to] || home_path
   end
   
   def after_update_path_for(resource)

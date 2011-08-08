e7d467fd16bd9242332750476e5a146f02298fa3
Stefan Wrobel <swrobel@gmail.com>
2011-08-08 12:42:14 -0700

    Rough cut of daily email put together

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,13 @@
 class UserMailer < ActionMailer::Base
   default :from => "Smartvark <noreply@smartvark.com>"
 
-  def daily_deals(user)   
+  def daily_deals(user)
+    food_cats = Category.find("feed-me").subtree.map(&:id)
+    @offer_1 = Offer.active.where(:category_id => food_cats).order("random()")
+    
+    other_cats = Category.where(:id - food_cats).map(&:id)
+    @offer_2 = Offer.active.where(:category_id => other_cats).order("random()")
+    
     mail(:to => user.email, :from => "Smartvark Local Deals <deals@smartvark.com>", :subject => "Your personalized daily deal picks from Smartvark")  
   end
 end

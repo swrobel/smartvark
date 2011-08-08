79ea4a3807cb17ca05d0d4ebc2b4fd45999af742
Stefan Wrobel <swrobel@gmail.com>
2011-08-08 12:46:43 -0700

    Fix mailer offer selection

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,10 +3,10 @@ class UserMailer < ActionMailer::Base
 
   def daily_deals(user)
     food_cats = Category.find("feed-me").subtree.map(&:id)
-    @offer_1 = Offer.active.where(:category_id => food_cats).order("random()")
+    @offer_1 = Offer.active.where(:category_id => food_cats).order("random()").limit(1).first
     
     other_cats = Category.where(:id - food_cats).map(&:id)
-    @offer_2 = Offer.active.where(:category_id => other_cats).order("random()")
+    @offer_2 = Offer.active.where(:category_id => other_cats).order("random()").limit(1).first
     
     mail(:to => user.email, :from => "Smartvark Local Deals <deals@smartvark.com>", :subject => "Your personalized daily deal picks from Smartvark")  
   end

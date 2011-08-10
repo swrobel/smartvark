702eeba2fa764d523a2b385661a1e7235567f196
Stefan Wrobel <swrobel@gmail.com>
2011-08-09 17:26:54 -0700

    Specify that yipit import data is in UTC

--- app/models/import.rb
+++ app/models/import.rb
@@ -4,59 +4,61 @@ class Import < ActiveRecord::Base
   has_many :yipit_rows
 
   def from_yipit(division="los-angeles")
-    user = User.find_by_email("api@yipit.com")
-    offer_type_id = OfferType.find_by_name("Coupon").id
-    begin
-      result = Net::HTTP.get URI.parse("http://api.yipit.com/v1/deals/?key=tFkn69TXrYFuHZgC&division=#{division}&limit=5000&paid=1")
-      data = Yajl::Parser.parse(result)
-    rescue Exception => ex
-      self.import_errors = ex
-      return
-    end
-    self.source_rows = data["response"]["deals"].count
-    self.success_rows = 0
-    data["response"]["deals"].each { |deal|
-      biz = deal["business"]
-      business_ids = []
-      row_errors = []
-      created_biz = false
+    Time.use_zone("UTC") do
+      user = User.find_by_email("api@yipit.com")
+      offer_type_id = OfferType.find_by_name("Coupon").id
+      begin
+        result = Net::HTTP.get URI.parse("http://api.yipit.com/v1/deals/?key=tFkn69TXrYFuHZgC&division=#{division}&limit=5000&paid=1")
+        data = Yajl::Parser.parse(result)
+      rescue Exception => ex
+        self.import_errors = ex
+        return
+      end
+      self.source_rows = data["response"]["deals"].count
+      self.success_rows = 0
+      data["response"]["deals"].each { |deal|
+        biz = deal["business"]
+        business_ids = []
+        row_errors = []
+        created_biz = false
 
-      row_errors << "No locations for business" if biz["locations"] == []
+        row_errors << "No locations for business" if biz["locations"] == []
 
-      biz["locations"].each { |loc|
-        begin
-          business = Business.find_by_yipit_id(loc["id"])
-          unless business
-            business = user.businesses.create!(yipit_id: loc["id"], name: biz["name"], address: loc["address"], city: loc["smart_locality"], state: loc["state"], zipcode: loc["zip_code"], phone: loc["phone"], website: biz["url"])
-            logger.info business
-            created_biz = true
+        biz["locations"].each { |loc|
+          begin
+            business = Business.find_by_yipit_id(loc["id"])
+            unless business
+              business = user.businesses.create!(yipit_id: loc["id"], name: biz["name"], address: loc["address"], city: loc["smart_locality"], state: loc["state"], zipcode: loc["zip_code"], phone: loc["phone"], website: biz["url"])
+              logger.info business
+              created_biz = true
+            end
+            business_ids << business.id
+          rescue Exception => ex
+            row_errors << ex
           end
-          business_ids << business.id
+        }
+        begin
+          offer = Offer.find_by_yipit_id(deal["id"])
+          created_offer = false
+          unless offer || business_ids == []
+            if deal["tags"] == []
+              row_errors << "No categories for offer"
+            else
+              category_id = YipitCategory.find_by_yipit_slug(deal["tags"].first["slug"]).category_id
+              offer = user.offers.create!(yipit_id: deal["id"], offer_type_id: offer_type_id, category_id: category_id, business_ids: business_ids, title: deal["yipit_title"], description: deal["title"], start_date: deal["date_added"], end_date: deal["end_date"], redemption_link: deal["url"], source: deal["source"]["name"], image_url_big: deal["images"]["image_big"], image_url_small: deal["images"]["image_small"])
+              created_offer = true
+            end
+          end
+          self.success_rows += 1 if offer
         rescue Exception => ex
           row_errors << ex
+        ensure
+          offer_id = offer.nil? ? nil : offer.id
+          row_errors = nil if row_errors == []
+          self.yipit_rows.create(yipit_id: deal["id"], offer_id: offer_id, created_offer: created_offer, created_biz: created_biz, row_data: deal, row_errors: row_errors)
         end
       }
-      begin
-        offer = Offer.find_by_yipit_id(deal["id"])
-        created_offer = false
-        unless offer || business_ids == []
-          if deal["tags"] == []
-            row_errors << "No categories for offer"
-          else
-            category_id = YipitCategory.find_by_yipit_slug(deal["tags"].first["slug"]).category_id
-            offer = user.offers.create!(yipit_id: deal["id"], offer_type_id: offer_type_id, category_id: category_id, business_ids: business_ids, title: deal["yipit_title"], description: deal["title"], start_date: deal["date_added"], end_date: deal["end_date"], redemption_link: deal["url"], source: deal["source"]["name"], image_url_big: deal["images"]["image_big"], image_url_small: deal["images"]["image_small"])
-            created_offer = true
-          end
-        end
-        self.success_rows += 1 if offer
-      rescue Exception => ex
-        row_errors << ex
-      ensure
-        offer_id = offer.nil? ? nil : offer.id
-        row_errors = nil if row_errors == []
-        self.yipit_rows.create(yipit_id: deal["id"], offer_id: offer_id, created_offer: created_offer, created_biz: created_biz, row_data: deal, row_errors: row_errors)
-      end
-    }
-  return nil
+    end
+    return nil
   end
 end

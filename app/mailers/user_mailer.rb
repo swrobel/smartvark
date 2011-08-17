b64db6427db43d8bb42becd1b0b1a68276ecd5b2
Stefan Wrobel <swrobel@gmail.com>
2011-08-17 00:43:23 -0700

    Update mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,14 @@ class UserMailer < ActionMailer::Base
   default :from => "Smartvark <noreply@smartvark.com>"
 
   def daily_deals(user)
+    @user = user
+
     food_cats = Category.find("feed-me").subtree.map(&:id)
-    @offer_1 = Offer.active.where(:category_id => food_cats).order("random()").limit(1).first
+    @offer_food = Offer.active.where(:category_id => food_cats).order("random()").limit(1).first
     
     other_cats = Category.where(:id - food_cats).map(&:id)
-    @offer_2 = Offer.active.where(:category_id => other_cats).order("random()").limit(1).first
+    @offer_other = Offer.active.where(:category_id => other_cats).order("random()").limit(1).first
     
-    mail(:to => user.email, :from => "Smartvark Local Deals <deals@smartvark.com>", :subject => "Your personalized daily deal picks from Smartvark")  
+    mail(:to => user.email, :from => "Smartvark Deals <deals@smartvark.com>", :subject => "Your personalized daily deal picks from Smartvark")  
   end
 end

e0cb9676b68c5fe12fd850c9274633cf7b15dbc5
Stefan Wrobel <swrobel@gmail.com>
2011-08-17 23:40:36 -0700

    Add test emails to cron task

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,13 @@
 class UserMailer < ActionMailer::Base
   default :from => "Smartvark <noreply@smartvark.com>"
 
-  def daily_deals(user)
-    @user = user
-
+  def daily_deals(email)
     food_cats = Category.find("feed-me").subtree.map(&:id)
     @offer_food = Offer.active.where(:category_id => food_cats).order("random()").limit(1).first
     
     other_cats = Category.where(:id - food_cats).map(&:id)
     @offer_other = Offer.active.where(:category_id => other_cats).order("random()").limit(1).first
     
-    mail(:to => user.email, :from => "Smartvark Deals <deals@smartvark.com>", :subject => "Your personalized daily deal picks from Smartvark")  
+    mail(:to => email, :from => "Smartvark Deals <deals@smartvark.com>", :subject => "Your personalized daily deal picks from Smartvark")  
   end
 end
--- lib/tasks/cron.rake
+++ lib/tasks/cron.rake
@@ -4,4 +4,8 @@ task :cron => :environment do
   import.from_yipit
   import.save
   Offer.archive_expired
+  emails = ["swrobel@gmail.com","arhodes@rhombuscreative.com","jstandard@gmail.com","melissa.pena11@gmail.com","panisa.suwanarat.2011@anderson.ucla.edu"]
+  emails.each do |email|
+    UserMailer.daily_deals(email).deliver
+  end
 end

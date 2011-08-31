8e98c64456395b9b4554cf2a41e64608bf1645c9
Stefan Wrobel <swrobel@gmail.com>
2011-08-30 21:42:23 -0700

    Fix ip location error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -81,7 +81,7 @@ class WelcomeController < ApplicationController
     
     @out_of_area = false
     loc ||= geo_location
-    @formatted_location ||= [[request.location.city, request.location.region_code].join(', '), request.location.zipcode].join(' ').strip unless Rails.env.development?
+    @formatted_location ||= [[request.location.city, request.location.state_code].join(', '), request.location.postal_code].join(' ').strip unless Rails.env.development?
     @formatted_location ||= LA.formatted_address
     if Geocoder::Calculations.distance_between(LA.coordinates,loc) > 50
       @out_of_area = true

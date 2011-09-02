56ded32154c99725d032dbee9daa3247ce959aeb
Stefan Wrobel <swrobel@gmail.com>
2011-09-02 11:32:19 -0700

    Update import code to capture backtraces

--- app/models/import.rb
+++ app/models/import.rb
@@ -20,7 +20,7 @@ class Import < ActiveRecord::Base
           row_errors = []
           created_biz = false
 
-          row_errors << "No locations for business" if !deal["locations"] || deal["locations"] == []
+          row_errors << {message: "No locations for business"} if !deal["locations"] || deal["locations"] == []
 
           deal["locations"].each { |loc|
             begin
@@ -31,15 +31,15 @@ class Import < ActiveRecord::Base
                 created_biz = true
               end
               business_ids << business.id
-            rescue Exception => ex
-              row_errors << ex
+            rescue => ex
+              row_errors << {message: ex.message, backtrace: ex.backtrace}
             end
           } if deal["locations"]
           offer = Offer.find_by_sqoot_id(deal["id"])
           created_offer = false
           unless offer || business_ids == []
             if !deal["categories"] || deal["categories"] == []
-              row_errors << "No categories for offer"
+              row_errors << {message: "No categories for offer"}
             else
               category_id = SqootCategory.find_by_name(deal["categories"].first).category_id
               start_date = Date.today
@@ -52,16 +52,16 @@ class Import < ActiveRecord::Base
             end
           end
           self.success_rows += 1 if offer
-        rescue Exception => ex
-          row_errors << ex
+        rescue => ex
+          row_errors << {message: ex.message, backtrace: ex.backtrace}
         ensure
           offer_id = offer.nil? ? nil : offer.id
           row_errors = nil if row_errors == []
           self.sqoot_rows.create(sqoot_id: deal["id"], offer_id: offer_id, created_offer: created_offer, created_biz: created_biz, row_data: deal, row_errors: row_errors)
         end
       }
-    rescue Exception => ex
-      self.import_errors = ex
+    rescue => ex
+      self.import_errors = {message: ex.message, backtrace: ex.backtrace}
       return false
     end
     return true

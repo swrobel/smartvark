2c448066bd18bea762a33fa5c03f6da1eedaa8ee
Stefan Wrobel <swrobel@gmail.com>
2011-09-12 07:45:54 -0700

    Fix bug with kgb deals workaround

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -95,7 +95,7 @@ class WelcomeController < ApplicationController
     raise CanCan::AccessDenied unless can? :read, :viewdeal
     @offer = Offer.find(params[:id])
     raise "Deals cannot be accessed by ID" unless @offer.friendly_id_status.friendly?
-    if @offer.source.downcase.include?("kgb")
+    if @offer.source.try(:downcase).try(:include?,"kgb")
       redirect_to @offer.redemption_link
     else
       render :layout => false

791e9bab46484b90adefe1f0a0620c983ff7bb4b
Stefan Wrobel <swrobel@gmail.com>
2011-09-19 18:07:45 -0700

    Add featured deal to mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,15 @@
 class UserMailer < ActionMailer::Base
   default :from => "Smartvark <noreply@smartvark.com>"
 
-  def daily_deals(user)
+  def daily_deals(user, exclusive=nil)
     @user = user
+    @exclusive = exclusive
 
     food_cats = Category.find("feed-me").subtree.map(&:id)
-    @offer_food = Offer.active.where(:category_id => food_cats).order("random()").limit(1).first
+    @offer_food = Offer.active.where({:category_id => food_cats} & (:source ^ nil) & (:image_url_small ^ nil)).order("random()").limit(1).first
     
     other_cats = Category.where(:id - food_cats).map(&:id)
-    @offer_other = Offer.active.where(:category_id => other_cats).order("random()").limit(1).first
+    @offer_other = Offer.active.where({:category_id => other_cats} & (:source ^ nil) & (:image_url_small ^ nil)).order("random()").limit(1).first
     
     subject = "Today's Deal Battle: " + @offer_food.businesses.first.name + " vs " + @offer_other.businesses.first.name
     mail(:to => user.email, :from => "Smartvark Deals <deals@smartvark.com>", :subject => subject)  
--- lib/tasks/cron.rake
+++ lib/tasks/cron.rake
@@ -9,10 +9,11 @@ task :cron => :environment do
   Offer.archive_expired
   
   puts "Cron: Email users @ #{Time.now}"
-  UserMailer.daily_deals(User.find_by_email("swrobel@gmail.com")).deliver
+  exclusive = Offer.find(85)
+  UserMailer.daily_deals(User.find_by_email("swrobel@gmail.com"), exclusive).deliver
   # users = User.near("Los Angeles, CA",40).where(:role => "user")
   # users.each do |user|
-  #   UserMailer.daily_deals(user).deliver
+  #   UserMailer.daily_deals(user, exclusive).deliver
   # end
 
   puts "Cron: Done @ #{Time.now}"

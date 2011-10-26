a8b9263b971382bd2ed35a9deb3b1eaec4950cf3
Stefan Wrobel <swrobel@gmail.com>
2011-10-26 15:31:57 -0700

    Add First Friday signups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111023234905) do
+ActiveRecord::Schema.define(:version => 20111024225923) do
 
   create_table "businesses", :force => true do |t|
     t.integer  "user_id"

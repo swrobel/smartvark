e1f6e3c9cf4f48d1005fc8aa55f6d030beda06d3
Stefan Wrobel <swrobel@gmail.com>
2013-02-28 06:50:12 -0800

    Switch back go Google Geocoder since Yahoo sucks and charges now

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,8 +1,8 @@
-#Geocoder::Configuration.lookup = :google
-#Geocoder::Configuration.api_key = ENV['GOOGLE_GEOCODER_KEY']
+Geocoder::Configuration.lookup = :google
+Geocoder::Configuration.api_key = ENV['GOOGLE_GEOCODER_KEY']
 
-Geocoder::Configuration.lookup = :yahoo
-Geocoder::Configuration.api_key = ENV['YAHOO_GEOCODER_KEY']
+#Geocoder::Configuration.lookup = :yahoo
+#Geocoder::Configuration.api_key = ENV['YAHOO_GEOCODER_KEY']
 
 #Geocoder::Configuration.lookup = :bing
 #Geocoder::Configuration.api_key = ENV['BING_GEOCODER_KEY']

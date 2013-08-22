167321257250d5ca7cd3da6f59760f28adc9fce3
Stefan Wrobel <swrobel@server.fake>
2013-08-22 13:23:08 -0700

    Google Geocoder API doesn't require a key anymore

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,10 +1,3 @@
 Geocoder::Configuration.lookup = :google
-Geocoder::Configuration.api_key = ENV['GOOGLE_GEOCODER_KEY']
-
-#Geocoder::Configuration.lookup = :yahoo
-#Geocoder::Configuration.api_key = ENV['YAHOO_GEOCODER_KEY']
-
-#Geocoder::Configuration.lookup = :bing
-#Geocoder::Configuration.api_key = ENV['BING_GEOCODER_KEY']
 
 LA = Geocoder.search("Los Angeles, CA").first
